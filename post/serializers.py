035d65df25ecdad5e80e3243d35e4475a9f2d989
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-19 18:19:58 +0300

    clean code

--- post/serializers.py
+++ post/serializers.py
@@ -2,12 +2,9 @@ from .models import Post
 from rest_framework import serializers
 
 class PostSerializer(serializers.ModelSerializer):
-
-    image_url = serializers.SerializerMethodField('get_image_url')
-
     class Meta:
         model = Post
-        fields = ('id', 'title', 'description', 'image', 'image_url')
+        fields = ('id', 'title', 'description', 'image')
 
     def get_image_url(self, obj):
         request = self.context.get('request')
