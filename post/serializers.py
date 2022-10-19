74d229aa66662888ca2f48a069d407996995bfe1
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-19 18:17:03 +0300

    add absolute url

--- post/serializers.py
+++ post/serializers.py
@@ -1,9 +1,14 @@
-from dataclasses import field
-from pyexpat import model
 from .models import Post
 from rest_framework import serializers
 
 class PostSerializer(serializers.ModelSerializer):
+
+    image_url = serializers.SerializerMethodField('get_image_url')
+
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ('id', 'title', 'description', 'image', 'image_url')
+
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.image.url)
