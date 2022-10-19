675e63c030d6449f7a152edf9f11775256e599d0
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-19 11:51:25 +0300

    create post && get endpoints

--- post/views.py
+++ post/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import imp
+import stat
+from .models import Post
+from .serializers import PostSerializer
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def get_posts(request):
+
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many = True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    if request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
